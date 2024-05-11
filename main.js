3c24c81b6000748030def99b444ca16f6f96959d
Steve-p-Dev <166451213+Steve-p-Dev@users.noreply.github.com>
2024-05-11 14:11:10 -0500

    proyecto terminado
    pulimos tema de sacar la carta a algun lado de la ventana y cuando de click a la misma esta vuelva a su contenedor original

--- main.js
+++ main.js
@@ -39,3 +39,46 @@ $(document).ready(function() {
 function toggleCard() {
     document.querySelector('.envelope').classList.toggle('open');
 }
+
+//drag and drog normal
+// $(function() {
+//     $(".letter").draggable({
+//         containment: "window"
+//     });
+// });
+
+
+//drag and drog pero arrastra y suelta se va a su posicion inicial
+// $(function() {
+//     $(".letter").draggable({
+//         containment: "window",
+//         revert: function(event, ui) {
+//             // on false, the widget will revert to its original position when dragging stops
+//             return !event;
+//         }
+//     });
+
+//     $(".letter").on("click", function() {
+//         $(this).animate({
+//             top: 0,
+//             left: 0
+//         });
+//     });
+// });
+
+
+//Drag and drog pero cuando haga arrastre y suelte en alguyn lado de la ventana esta se queda y vuelve cuandode le click
+
+$(function() {
+    $(".letter").draggable({
+        containment: "window"
+    });
+
+    $(".letter").on("click", function() {
+        $(this).animate({
+            top: 0,
+            left: 0
+        });
+    });
+});
+
